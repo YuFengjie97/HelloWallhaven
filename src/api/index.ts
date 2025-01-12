36f1d5a865840955cbd7248aab2f7b8f95ec083a
yufengjie <2457522528@qq.com>
2025-01-12 13:07:31 +0800

    ts

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+
+export const instance = axios.create({
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+  },
+  timeout: 10000
+})
+
+
+export function login(data: { username: string, password: string, _token: string }) {
+  return instance.post('https://wallhaven.cc/auth/login', data)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 import readlineSync from 'readline-sync'
+import { login, instance } from './api';
+import user from '../user.json'
 
-function login() {
-  const username = readlineSync.question('输入用户名')
-  console.log('hello ', username);
+async function testNet() {
+  const res = await instance.get('https://wallhaven.cc/')
+  if(res.status === 200) {
+    return true
+  }
+  return false
 }
 
-login()
+async function goLogin() {
+  // const username = readlineSync.question('input username: ')
+  // const password = readlineSync.question('input password: ')
+  const username = user.username
+  const password = user.password
+  const _token = '6JYAlqB4ldB0Ce197oqFXrJ06yoyIuLHAlnIxkzn'
+  console.log({ username, password });
+  // try{
+  //   const res = await login({ username, password, _token })
+  //   console.log(res.status);
+  // }catch(e){
+  //   console.log(e);
+  // }
+}
+
+
+; (async () => {
+  const isNetWork = await testNet()
+  if(isNetWork){
+    console.log('网络可用');
+  }else{
+    console.log('网络不可用,需要科学上网');
+    return
+  }
+
+  await goLogin()
+})()
